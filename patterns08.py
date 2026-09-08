4f2da5b8ef22f457e62324f0814bd08eea569afd
Sumon Chandro Shil <120047714+sumon-chandro-shil@users.noreply.github.com>
2026-09-08 21:04:01 +0600

    Pattern 8 Done.

---
+++ patterns08.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):
+    for j in range(n, 0, -1):
+        print("*", end="")
+    print()
+
+"""
+n = 5
+Output:
+*****
+*****
+*****
+*****
+*****
+"""
