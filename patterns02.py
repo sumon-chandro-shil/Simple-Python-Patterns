80a21eb9f967ff6d815ffc9f3949ec50aac5cc7a
Sumon Chandro Shil <120047714+sumon-chandro-shil@users.noreply.github.com>
2026-09-08 20:46:27 +0600

    Pattern 2 Done.

---
+++ patterns02.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print(i, end="")
+    print()
+
+"""
+n = 5
+output:
+11111
+22222
+33333
+44444
+55555
+"""
