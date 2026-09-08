82cb30d04652d2bf247a96ca98a26c41438cc5ef
Sumon Chandro Shil <120047714+sumon-chandro-shil@users.noreply.github.com>
2026-09-08 20:48:49 +0600

    Pattern 3 Done.

---
+++ patterns03.py
@@ -0,0 +1,15 @@
+n = int(input("Enter a number: "))
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        print("*", end="")
+    print()
+
+"""
+n = 5
+output:
+*****
+*****
+*****
+*****
+*****
+"""
