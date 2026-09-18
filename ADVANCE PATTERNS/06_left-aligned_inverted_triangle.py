790982afb7cd680c5c3f06f5c48516918000bd7a
Sumon Chandro Shil <120047714+sumon-chandro-shil@users.noreply.github.com>
2026-09-19 00:13:44 +0600

    LEFT-ALIGNED INVERTED TRIANGLE DONE.

---
+++ ADVANCE PATTERNS/06_left-aligned_inverted_triangle.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+"""
+Enter a number: 5
+5 4 3 2 1 
+4 3 2 1 
+3 2 1 
+2 1 
+1 
+"""
