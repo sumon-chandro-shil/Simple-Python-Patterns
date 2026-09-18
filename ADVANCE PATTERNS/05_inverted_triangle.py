836a720b489e6766fd0755cbe01ddc43e7aaf3a7
Sumon Chandro Shil <120047714+sumon-chandro-shil@users.noreply.github.com>
2026-09-19 00:09:10 +0600

    INVERTED TRIANGLE DONE.

---
+++ ADVANCE PATTERNS/05_inverted_triangle.py
@@ -0,0 +1,14 @@
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ")
+    print()
+
+"""
+Enter a number: 5
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1
+"""
